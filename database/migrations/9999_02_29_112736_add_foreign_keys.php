6b2340cbec8ffbdd7ea19b086c0dab33ef2e5cb4
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-29 12:45:12 +0100

    add: aggiunto migration

---
+++ database/migrations/9999_02_29_112736_add_foreign_keys.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('event_tag', function (Blueprint $table) {
+            $table->foreignId('event_id')->constrained();
+            $table->foreignId('tag_id')->constrained();
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('event_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('event_tag', function (Blueprint $table) {
+            $table->dropForeign('event_tag_event_id_foreign');
+            $table->dropColumn('event_id');
+            $table->dropForeign('event_tag_tag_id_foreign');
+            $table->dropColumn('tag_id');
+        });
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign('users_event_id_foreign');
+            $table->dropColumn('event_id');
+        });
+    }
+};
