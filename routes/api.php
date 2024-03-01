7d963e6053fc315be27f158033991ee9ee0b67d0
Massidama <massidama@outlook.it>
2024-03-01 17:24:37 +0100

    aggiunta chiamate axios in front-end

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\ApiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +17,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route :: group(['prefix' => '/v1'], function() {
+
+    Route :: get('events', [ ApiController :: class, 'getEvents']);
+    Route :: get('events/{id}', [ ApiController :: class, 'singleEvent']);
+    
 });
